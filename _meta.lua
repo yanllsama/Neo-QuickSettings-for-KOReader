162e729789c07582b9e774a6600f697fd455f51b
A.KAYA <akyidil@gmail.com>
2026-08-25 22:17:34 +0300

    Update _meta.lua

--- _meta.lua
+++ _meta.lua
@@ -2,5 +2,5 @@ return {
     name = "Neo Quick Settings",
     fullname = "Neo Quick Settings Panel",
     description = "Quick Settings panel tab with action buttons, brightness and warmth sliders, and custom button support.",
-    version = 1,
+    version = "1.20",
 }
