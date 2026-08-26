4e389f30a1afc01e29959994768ff61519da37ea
A.KAYA <akyidil@gmail.com>
2026-08-26 13:09:05 +0300

    Delete _meta.lua

--- _meta.lua
+++
@@ -1,6 +0,0 @@
-return {
-    name = "Neo Quick Settings",
-    fullname = "Neo Quick Settings Panel",
-    description = "Quick Settings panel tab with action buttons, brightness and warmth sliders, and custom button support.",
-    version = "1.20",
-}
