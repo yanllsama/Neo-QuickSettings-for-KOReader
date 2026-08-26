e1cd64046f41113be0aa854e545313a9c2a3bb52
A.KAYA <akyidil@gmail.com>
2026-08-26 13:38:39 +0300

    delete files
    all files

--- plugins/neo_quicksetting.koplugin/_meta.lua
+++
@@ -1,7 +0,0 @@
-local _ = require("gettext")
-return {
-    name        = "neo_quicksetting",
-    fullname    = "Neo Quick Settings",
-    version     = "1.20",
-    description = _([[Customizable quick settings panel with action buttons, brightness/warmth sliders and custom icon support.]]),
-}
