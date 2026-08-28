3a52d0d5668837a22336314ba376d991799b989e
A.KAYA <akyidil@gmail.com>
2026-08-28 22:42:45 +0300

    Update _meta.lua
    Update _meta.lua

--- plugins/neo_quicksetting.koplugin/_meta.lua
+++ plugins/neo_quicksetting.koplugin/_meta.lua
@@ -2,6 +2,6 @@ local _ = require("gettext")
 return {
     name        = "neo_quicksetting",
     fullname    = "Neo Quick Settings",
-    version     = "1.20",
+    version     = "1.21",
     description = _([[Customizable quick settings panel with action buttons, brightness/warmth sliders and custom icon support.]]),
 }
